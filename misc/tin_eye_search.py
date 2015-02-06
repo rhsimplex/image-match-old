04fd4bca59b9392cea5e185a29073165eb399ada
rhsimplex <hendersonry@gmail.com>
2015-02-06 11:50:16 +0100

    used tineye api, tested

--- misc/tin_eye_search.py
+++ misc/tin_eye_search.py
@@ -1,28 +1,32 @@
-import requests
-import time
-
+from pytineye import TinEyeAPIRequest
 """
 Similarity search implemented via the tineye API.
 
-Uses the requests library: http://docs.python-requests.org/en/latest/
+Uses the pytineye library: https://github.com/TinEye/pytineye
+
+Note: local system time must be more or less accurate for this to work, which
+can be an issue on the VMs. If all else fails, use 'sudo date -s JAN 2015 18:00:00'
+with the current date and time to fix it.  You don't have to be accurate to the second.
 
 """
 
-URL = 'http://api.tineye.com/rest/search/'
+URL = 'http://api.tineye.com/rest/'
 public_key = 'dT-Q98^ICh-H8AboWSMQ'
 private_key_path = 'private_key.txt'
 
 
 def similarity_search(path, limit=10):
     with open(private_key_path) as f:
-        args = {
-            'public key': public_key,
-            'private key': f.read(),
-            'limit': str(limit),
-            'image data filename': path,
-            'date': str(int(time.time()))
-        }
+        with open(path, 'rb') as img:
+            private_key = f.read()
 
-        files = {'file': open(path, 'rb')}
+            api = TinEyeAPIRequest(URL, public_key, private_key)
 
-        r = requests.post(URL, data=args, files=files)
+            response = api.search_data(img.read(), limit=limit)
+
+            if response.matches:
+                return {'verdict': 'fail',
+                        'reason': [match.backlinks[0].url for match in response.matches]}
+            else:
+                return {'verdict': 'pass',
+                        'reason': []}
