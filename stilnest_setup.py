fec807e30427f165873d2b2b2a78b4bf55b29c5c
ryan <hendersonry@gmail.com>
2015-03-05 16:43:31 +0100

    small optimizations

--- stilnest_setup.py
+++ stilnest_setup.py
@@ -1,8 +1,40 @@
 from image_match.signature_database import SignatureCollection
 import sys
+import requests
 from pymongo import MongoClient
-from os import walk
-from os.path import join
+from os import walk, spawnvp, P_WAIT, remove, listdir
+from os.path import join, split, abspath
+from time import time
+
+# you will need blender 2.7x! see install instructions here:
+# https://launchpad.net/~irie/+archive/ubuntu/blender
+class StilnestCollection(SignatureCollection):
+    def stilnest_lookup(self, stl_file_URL):
+        filename = split(stl_file_URL)[-1]
+        with open(filename, 'w+') as f:
+            download_start_time = time()
+            stl_data = requests.get(stl_file_URL).content
+            f.write(stl_data)
+
+        blender_start_time = time()
+        L = ['blender', '-b', '-P', abspath('misc/stl2images.py'), '--',
+             abspath(filename), abspath('.')]
+        spawnvp(P_WAIT, 'blender', L)
+        remove(filename)
+
+        database_start_time = time()
+        result = {}
+        for image_name in filter(lambda x: x.startswith(filename) and x.endswith('.png'),
+                            listdir('.')):
+            print image_name
+            result[image_name] = self.similarity_search(image_name, n_parallel_words=10, word_limit=20, all_orientations=True)
+            remove(image_name)
+        finish_time = time()
+        result['timing'] = {'download': blender_start_time - download_start_time,
+                          'blender': database_start_time - blender_start_time,
+                          'lookup': finish_time - database_start_time}
+        return result
+
 
 """
 Script to setup a database from Stilnest directory structure
