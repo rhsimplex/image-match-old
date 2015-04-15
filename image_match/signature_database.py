f4f0e8b9de72697c5d7da94ade5998bd827b45f6
ryan <hendersonry@gmail.com>
2015-04-15 19:00:54 +0200

    threads join properly

--- image_match/signature_database.py
+++ image_match/signature_database.py
@@ -296,8 +296,8 @@ class SignatureCollection(object):
                         l.append(results[key])
 
             # join children
-            # for process in p:
-            #     process.join()
+            for process in p:
+                process.join()
 
             # yield a set of results
             yield l
