7c23a6670d73753ebdf1062f10758dbab3491057
vrde <agranzot@gmail.com>
2015-04-16 12:06:57 +0200

    Remove useless print

--- image_match/web/base.py
+++ image_match/web/base.py
@@ -7,7 +7,6 @@ class RequestHandler(tornado.web.RequestHandler):
 
     def get_template_namespace(self):
         ns = super(RequestHandler, self).get_template_namespace()
-        print 'hello', settings.BASE_URL
         ns.update({
             'BASE_URL': settings.BASE_URL
         })
