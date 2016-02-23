c394d9a4add7c901abb8ed1590a83534dbe212f7
ryan <hendersonry@gmail.com>
2016-02-23 14:49:41 +0100

    whitespace

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'image_match',
-    version = '0.1',
-    packages = find_packages()
+    name='image_match',
+    version='0.1',
+    packages=find_packages()
 )
