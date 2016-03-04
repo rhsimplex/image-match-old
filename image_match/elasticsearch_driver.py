65c30e3c388ec1c7f2d8b65ca4206467cb2f351a
ryan <hendersonry@gmail.com>
2016-03-04 16:14:41 +0100

    more docstring goodness

--- image_match/elasticsearch_driver.py
+++ image_match/elasticsearch_driver.py
@@ -5,9 +5,44 @@ import numpy as np
 
 
 class SignatureES(SignatureDatabaseBase):
+    """Elasticsearch driver for image-match
+
+    """
 
     def __init__(self, es, index='images', doc_type='image', timeout=10, size=100,
                  *args, **kwargs):
+        """Extra setup for Elasticsearch
+
+        Args:
+            es (elasticsearch): an instance of the elasticsearch python driver
+
+            index (Optional[string]): a name for the Elasticsearch index (default 'images')
+
+            doc_type (Optional[string]): a name for the document time (default 'image')
+
+            timeout (Optional[int]): how long to wait on an Elasticsearch query, in seconds (default 10)
+
+            size (Optional[int]): maximum number of Elasticsearch results (default 100)
+
+            *args (Optional): Variable length argument list to pass to base constructor
+
+            **kwargs (Optional): Arbitrary keyword arguments to pass to base constructor
+
+        Examples:
+            >>> from elasticsearch import Elasticsearch
+            >>> from image_match.elasticsearch_driver import SignatureES
+            >>> es = Elasticsearch()
+            >>> ses = SignatureES(es)
+            >>> ses.add_image('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg/687px-Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg')
+            >>> ses.search_image('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg/687px-Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg')
+            [
+             {'dist': 0.0,
+              'id': u'AVM37nMg0osmmAxpPvx6',
+              'path': u'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg/687px-Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg',
+              'score': 0.28797293}
+            ]
+
+        """
         self.es = es
         self.index = index
         self.doc_type = doc_type
@@ -59,3 +94,4 @@ class SignatureES(SignatureDatabaseBase):
     def insert_single_record(self, rec):
         rec['timestamp'] = datetime.now()
         self.es.index(index=self.index, doc_type=self.doc_type, body=rec)
+
