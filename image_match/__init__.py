cebd8559f10bd7e7f77843e453e1210163134a00
Sylvain Bellemare <sylvain@ascribe.io>
2016-03-23 16:45:37 +0100

    Add version to __init__ and read it for setup

--- image_match/__init__.py
+++ image_match/__init__.py
@@ -1,1 +1,2 @@
 __author__ = 'ryan'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -8,8 +8,29 @@ cluster!
 Based on the paper An image signature for any kind of image, Goldberg et
 al <http://www.cs.cmu.edu/~hcwong/Pdfs/icip02.ps>.
 """
+import io
+import os
+import re
 
 from setuptools import setup, find_packages
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r'^__version__ = [\'"]([^\'"]*)[\'"]', version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
 
 tests_require = [
     'coverage',
@@ -36,7 +57,7 @@ docs_require = [
 
 setup(
     name='image_match',
-    version='0.1.0',
+    version=find_version('image_match', '__init__.py'),
     description='image_match is a simple package for finding approximate '\
                 'image matches from a corpus.',
     long_description=__doc__,
